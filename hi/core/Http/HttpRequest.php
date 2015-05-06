bc570c1310f03f8d4cbb62dd7c6a6214f426b2bc
diegomariani <diegomariani.com@gmail.com>
2015-05-06 23:09:08 +0200

    Added sanitize to $_POST $_GET

--- hi/core/Http/HttpRequest.php
+++ hi/core/Http/HttpRequest.php
@@ -13,14 +13,14 @@ class HttpRequest {
 	protected $POST_METHOD = 'POST';
 	
 	public function post($name) {
-
+		$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 		if(isset($_POST[$name])) return $_POST[$name];
 		return [];
 	
 	}
 	
 	public function get($name) {
-		
+		$_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
 		if(isset($_GET[$name])) return $_GET[$name];
 		return [];
 	
