e0b254c5527d6a4674253fa1d78d58b43a114404
Diego Mariani <info@diegomariani.com>
2015-06-26 03:05:22 +0200

    Updated autoloading using preg_match

--- bootstrap/autoloader.php
+++ bootstrap/autoloader.php
@@ -12,19 +12,13 @@ require_once dirname(__DIR__) . '/hi/core/lib/orm/rb.php';
  * @version: 0.0.1 2015-02-02
  *
  */
-function autoload($className) {
+function autoload($class) {
+    
     $root = dirname(dirname(__FILE__));
-    $className = ltrim($className, '\\');
-    $fileName  = '';
-    $namespace = '';
-    if ($lastNsPos = strrpos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-    require_once $root .'/'. $fileName;
+    
+    preg_match('/^(.*\\\\)(.*)/',  ltrim($class, '\\'), $match );
+    
+    require_once $root.'/'. str_replace( '\\', '/', $match[1] ). $match[2] . '.php';
 }
 
 spl_autoload_register('autoload');
